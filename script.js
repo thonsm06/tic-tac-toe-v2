a60a09149b45f674a25e214a70320648fd9e88f9
Smith Thongbai <thonsm06@gmail.com>
2023-06-02 14:09:50 -0500

    Added a preliminary way to display cells

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 (function() { 
     //-----Board Module Pattern-----//////////////////////////////////////////////
     const gameBoard = (() => {
-        let board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];//main board
+        let board = [[['1', ''], ['2', ''], ['3', '']], [['4', ''], ['5', ''], ['6', '']], [['7', ''], ['8', ''], ['9', '']]];//main board
         
         const getBoard = () => {
             return board;
         }
         const resetBoard = () => {
-            board = [['1', '2', '3'], ['4','5', '6'], ['7', '8', '9']];
+            board = [[['1', ''], ['2', ''], ['3', '']], [['4', ''], ['5', ''], ['6', '']], [['7', ''], ['8', ''], ['9', '']]];
         }
         const setValue = (i, j, val) => {
             board[i][j] = val;
@@ -18,25 +18,32 @@
     
     //-----Display Module Pattern-----//////////////////////////////////////////////
     const displayBoard = (() => {
-        const board = gameBoard.getBoard();
+        const createDom = () => {
+            const body = document.querySelector('body');
+            const board = gameBoard.getBoard();
 
-        board.forEach(element => {
-            element.forEach(element => {
-                
-            
-        })});
+            board.forEach(element => {
+                element.forEach(element => {
+                    element[1] = document.createElement('div');    
+                    element[1].style.border = '1px solid black';
+                    element[1].style.width = '32px';
+                    element[1].style.height = '32px';
+                    element[1].textContent = element[0];
 
-        return {board};
+                    body.appendChild(element[1]);
+            })});
+        }
+        return {createDom};
     })();
 
     //-----Player Factory-----//////////////////////////////////////////////////////
     const playerFactory = (name) => {
         return {name};
     }
-    
+    displayBoard.createDom();
     const player1 = playerFactory('Dude');
     console.log(player1.name);
-
+    console.log(gameBoard.getBoard());
 })();
 
 
