cc0d92200292bc019debaf8ea3475ff743ec0702
Smith Thongbai <thonsm06@gmail.com>
2023-06-03 14:12:25 -0500

    Added a template to compare solution.

--- script.js
+++ script.js
@@ -74,13 +74,82 @@
             const currentPlayer = symbol;
             const board = gameBoard.getBoard();
             //check the entire board, but only one row, column, and diagonal direction at a time
-            //if corner cell
+            
+            if((posX === 0 || posX === 2) && (posY === 0 || posY === 2)){//if corner cell
+                //check side and cross
+                
+            } else if ((posX === 1 && posY === /[0-2]/) || (posY === 1 && posX === /[0-2]/)) { //if side cell
+                //check T
+            } else if (posX === 1 && posY === 1){ //if middle cell
+                //check up/down and cross
+            }
 
-            //if side cell
+            row1 = [0, 0, 1, 0, 2, 0]; 
+            row2 = [0, 1, 1, 1, 2, 1];
+            row3 = [0, 2, 1, 2, 2, 2];
 
-            //if middle cell
-            if(element[i]) {
+            column1 = [0, 0, 0, 1, 0, 2];
+            column2 = [1, 0, 1, 1, 1, 2];
+            column3 = [2, 0, 2, 1, 2, 2];
 
+            cross1 = [0, 0, 1, 1, 2, 2]; //top left to bottom right
+            cross2 = [2, 0, 1, 1, 0, 2]; //top right to bottom left
+
+            topleft = [row1, column1, cross1];
+            topright = [row1, column3, cross2];
+            bottomleft = [row3, column1, cross2];
+            bottomright = [row3, column3, cross1];
+
+            topmid = [row1, column2];
+            leftmid = [row2, column1];
+            bottommid = [row3, column2];
+            leftmid = [row2, column3];
+
+            middle = [row2, column2, cross1, cross2];
+
+            //Pick one after an input is made
+            function choosentemplate() {
+                const templateArray = [];
+                return templateArray;
+            }
+
+
+            const len = templateArray.length;
+            const coords = [];
+            
+            for(let i = 0; i < len; i++) {
+                for(let j = 0; j < 6; j++) {
+                    coords.push(templateArray[i][j]);
+/*                     x1 = templateArray[i][0];
+                    y1 = templateArray[i][1];
+                    x2 = templateArray[i][2];
+                    y2 = templateArray[i][3];
+                    x3 = templateArray[i][4];
+                    y3 = templateArray[i][5]; */
+                    board
+                }
+
+            }
+            
+            function lineToCheck(x1, y1, x2, y2, x3, y3) {
+                //get textcontent of the three cell
+                cell1 = element[1].textContent;
+                cell2 = element[1].textContent;
+                cell3 = element[1].textContent;
+                if (cell1 == cell2 && cell1 == cell3){
+                    return true;
+                }
+                return false;
+            }
+            if (symbol1 == symbol2) {
+
+            }
+
+
+            for(let i = 0; i < 3; i++) {
+                for(let j = 0; j < 3; j++) {
+
+                }
             }
         }
 
